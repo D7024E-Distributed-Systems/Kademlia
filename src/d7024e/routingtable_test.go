785506f4b59284c70d77572b92055003886fa113
Hugo Pettersson <51755959+HugPet@users.noreply.github.com>
2022-09-02 17:06:43 +0200

    add more tests

--- src/d7024e/routingtable_test.go
+++ src/d7024e/routingtable_test.go
@@ -8,6 +8,11 @@ import (
 func TestRoutingTable(t *testing.T) {
 	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
 
+	closestIDs := []string{"2111111400000000000000000000000000000000",
+		"1111111400000000000000000000000000000000", "1111111100000000000000000000000000000000", "1111111200000000000000000000000000000000",
+		"1111111300000000000000000000000000000000", "ffffffff00000000000000000000000000000000",
+	}
+
 	rt.AddContact(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
 	rt.AddContact(NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
 	rt.AddContact(NewContact(NewKademliaID("1111111200000000000000000000000000000000"), "localhost:8002"))
@@ -18,5 +23,20 @@ func TestRoutingTable(t *testing.T) {
 	contacts := rt.FindClosestContacts(NewKademliaID("2111111400000000000000000000000000000000"), 20)
 	for i := range contacts {
 		fmt.Println(contacts[i].String())
+		if contacts[i].ID.String() != closestIDs[i] {
+			t.Fail()
+		}
+	}
+}
+
+func TestRandomID(t *testing.T) {
+	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+
+	rt.AddContact(NewContact(NewRandomKademliaID(), "localhost:8001"))
+
+	contacts := rt.FindClosestContacts(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), 20)
+
+	if len(contacts) != 1 {
+		t.Fail()
 	}
 }
---
+++ src/network/network_test.go
@@ -0,0 +1,17 @@
+package network
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/D7024E-Distributed-Systems/Kademlia/src/d7024e"
+)
+
+func TestNetworkStruct(t *testing.T) {
+	target := d7024e.NewRandomKademliaID()
+	contact := d7024e.NewContact(target, "127.0.0.1:3000")
+
+	network := NewNetwork(&contact)
+
+	fmt.Println(network.currentNode.ID)
+}
