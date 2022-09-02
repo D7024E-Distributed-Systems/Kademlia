4a46ae9023aefa3d992a0d98c23c68a9ae25e158
JohanOlsson99 <51018314+JohanOlsson99@users.noreply.github.com>
2022-09-02 17:09:14 +0200

    Added some random port and fetching of ip address, starts a server if server exists on port 3000

--- src/main.go
+++ src/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
 	"time"
 
 	. "github.com/D7024E-Distributed-Systems/Kademlia/src/d7024e"
@@ -11,15 +15,35 @@ func main() {
 	target := NewRandomKademliaID()
 	contact := NewContact(target, "127.0.0.1:3000")
 	contact.CalcDistance(target)
-	currentContact := NewContact(NewRandomKademliaID(), "127.0.0.1:3000")
+	currentContact := NewContact(NewRandomKademliaID(), "")
 	network := NewNetwork(&currentContact)
 	ping := network.SendPingMessage(&contact)
 	if ping {
+		ip := GetOutboundIP()
+		// port := rand.Int(65535-1024) + 1024
+		rand.Seed(time.Now().UnixNano())
+		port := rand.Intn(65535-1024) + 1024
+		fmt.Println(ip)
+		fmt.Println(port)
+		go Listen(ip.String(), port)
 		// TODO: Send find node to server and start listening on random port
 	} else {
 		go Listen("127.0.0.1", 3000)
-		for {
-			time.Sleep(2 * time.Second)
-		}
 	}
+	for {
+		time.Sleep(2 * time.Second)
+	}
+}
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
